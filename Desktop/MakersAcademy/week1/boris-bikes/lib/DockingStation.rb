6908243a9d31166931ebc045c839e08a53459c30
Faisal Mahmood <fahmahmood@gmail.com>
2016-05-26 16:40:15 +0100

    refactor SRP; PVT

--- Desktop/MakersAcademy/week1/boris-bikes/lib/DockingStation.rb
+++ Desktop/MakersAcademy/week1/boris-bikes/lib/DockingStation.rb
@@ -1,20 +1,38 @@
 require_relative 'Bike'
 
 class DockingStation
+	
+
 	attr_reader :bike 
 
+	
 	def initialize
 		@bikes = []
 	end 
 
+	
 	def release_bike
-		fail "No bikes available" if @bikes.empty?
+		fail "No bikes available" if empty?
 		@bikes.pop
 	end
 
+	
 	def dock(bike)
-		fail "No more spaces for bikes" if @bikes.count>=20
+		fail "No more spaces for bikes" if full?
 		@bikes << bike
+	end
+
+	private 
+
+	def full?
+		@bikes.count >=20 ? true : false 
+	end  
+
+	
+	def empty?
+		@bikes.count <= 0 ? true : false
 	end 
+
+
 
 end
